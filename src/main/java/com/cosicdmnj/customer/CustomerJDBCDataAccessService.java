a0f7e594aa02a729a673e79eef266da707ccb2fe
Damnjan <cosicdamnjan@gmail.com>
2023-11-12 00:05:38 +0100

    Added JDBC template

---
+++ src/main/java/com/cosicdmnj/customer/CustomerJDBCDataAccessService.java
@@ -0,0 +1,52 @@
+package com.cosicdmnj.customer;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jdbc")
+public class CustomerJDBCDataAccessService implements CustomerDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CustomerJDBCDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public boolean existsPersonWithId(Integer id) {
+        return false;
+    }
+
+    @Override
+    public void deleteCustomerById(Integer customerId) {
+
+    }
+
+    @Override
+    public void updateCustomer(Customer update) {
+
+    }
+}
